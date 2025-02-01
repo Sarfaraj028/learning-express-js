e80ac48d357193534adb42c11edf3b32fb5d7cb0
Sarfaraj028 <sarfaraja0869@gmail.com>
2025-02-01 15:28:24 +0530

    add wroking signup / login

--- login-signUp-day4/app.js
+++ login-signUp-day4/app.js
@@ -11,55 +11,50 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) =>{
-    res.render("home")
-})
-app.get('/login', (req, res) =>{
     res.render("login")
 })
-app.get('/sign-up', (req, res) =>{
+
+//post of user login
+app.post('/login', async (req, res) => {
+    try {
+        const {username, password} = req.body
+        const checkedUser = await userModel.findOne({ username });
+        
+        if (!checkedUser) {
+            return res.send("User not found");
+        }
+
+        if (checkedUser.password === password) {
+            res.render("home");
+        } else {
+            res.send("Invalid password");
+        }
+    } catch (error) {
+        res.status(500).send("Server error: " + error.message);
+    }
+});
+
+
+app.get('/sign-up', (req, res) =>{ 
     res.render("sign-up")
 })
 
-app.post('/', async (req, res) =>{
-    
-    const {username, password} = req.body;
-
-    const user = await userModel.create({
-        username: username,
-        password: password
-    })
-
-    // check user already exist or not 
-    const exstingUser = await userModel.findOne({
-        username: username
-    })
-    if(exstingUser){
-        res.send("already existing")
+//post when user register
+app.post('/sign-up', async (req, res) =>{ 
+    try {
+        const data = req.body
+        await userModel.create(data) //insertMany()
+        console.log("user created: ",data);
+        
+        res.render("home")
+    } catch (error) {
+        console.error("Error while creating new user", error);
+        res.status(500).json(
+            {
+                message: "user not created!"
+            }
+        )
     }
-    else{
-        res.render("login")
-    }
-
-    console.log(user);
-    
-    res.render('home')
-})
-app.post('/', async (req, res) =>{
-
-    const {username, password} = req.body;
-
-    const user = await userModel.findOne({
-        username: 'saif'
-    })
-
-    if(user.password === password){
-        res.render('home')
-    }
-    else{
-        res.send("User not found")
-    } 
-
-    console.log(user);
 })
 
 app.listen(8000, () =>{
